162941ddb410cb07b27ae4f25a56218a31a0c33e
cubasve <eva.cubasvasquez@gmail.com>
2020-10-30 22:48:10 -0400

    Add categoryURL to back-end GET

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-const jokeURL = 'https://api.chucknorris.io/jokes/random';
+const jokeURL = 'https://api.chucknorris.io/jokes/random?category=';
+const categoryURL = 'https://api.chucknorris.io/jokes/categories';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  const category = req.query.category;
 
-  const options = {
-    url: `${jokeURL}`,
-    headers: { 'User-Agent': 'cubasve' }
-  }
+  request(categoryURL, function (err, response, body) {
+    const chooseCategory = JSON.parse(body);
+    if (category) {
+      request(`${jokeURL}/category`, function (err, response, body) {
+        const joke = JSON.parse(body);
+        res.render('index', { category: category, chooseCategory: chooseCategory, joke: joke.value });
+      });
+    } else {
+      res.render('index', { category: category, chooseCategory: null, joke: null })
+    }
+  })
 
-  request(options, function (err, response, body) {
-    const joke = JSON.parse(body);
-    res.render('index', { joke });
-  });
+  // const options = {
+  //   url: `${jokeURL}`,
+  //   headers: { 'User-Agent': 'cubasve' }
+  // }
+
+  // request(options, function (err, response, body) {
+  //   const joke = JSON.parse(body);
+  //   res.render('index', { joke });
+  // });
 
 });
 
