d4c21685dfa2743c729dc7cbe1aea14ae39a32f4
cubasve <eva.cubasvasquez@gmail.com>
2020-10-30 22:35:06 -0400

    Add options and joke.value for button to work

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-const jokeURL = 'https://api.chucknorris.io/jokes/random?category=/';
+const jokeURL = 'https://api.chucknorris.io/jokes/random';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  request(jokeURL, function (err, response, body) {
+  const options = {
+    url: `${jokeURL}`,
+    headers: { 'User-Agent': 'cubasve' }
+  }
+
+  request(options, function (err, response, body) {
     const joke = JSON.parse(body);
-    res.render('index', { joke: joke });
+    res.render('index', { joke });
   });
 
 });
